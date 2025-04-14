baf2a991fda573046cd5a702d2c19fa78458cfb8
DomPatchara <domdoo2015@gmail.com>
2025-04-14 17:17:52 +0700

    add

--- src/components/MovieInput.jsx
+++ src/components/MovieInput.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { addMovie } from "../redux/movieSlice"; 
+import { addMovie } from "../redux/movieSlice";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 
 const MovieInput = () => {
-
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [newMovie, setNewMovie] = useState("");
 
   const handleAddMovie = () => {
     if (newMovie) {
-        dispatch(addMovie(newMovie));
-        setNewMovie("");
+      dispatch(addMovie(newMovie));
+      setNewMovie("");
     }
-  }
+  };
   return (
     <div className="flex gap-2">
       <input
@@ -22,9 +21,10 @@ const MovieInput = () => {
         type="text"
         className="border-2 rounded-2xl focus:outline-none px-3 py-2"
       />
-      <button 
+      <button
         onClick={handleAddMovie}
-        className="px-4 py-2 bg-gray-900 rounded-2xl text-white/80 hover:bg-gray-800">
+        className="px-4 py-2 bg-gray-900 rounded-2xl text-white/80 hover:bg-gray-800"
+      >
         Add Movie
       </button>
     </div>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -10,24 +10,24 @@ const MovieList = () => {
 
   const handleRemoveMovie = (id) => {
     dispatch(removeMovie(id));
-  }
+  };
 
   return (
     <div>
       <h1 className="text-5xl font-bold text-gray-900 mb-2">Movie List</h1>
       <div className="space-y-2">
         {movies.map((movie) => (
-            <div key={movie.id} className="flex gap-4">
+          <div key={movie.id} className="flex gap-4">
             <div className="text-2xl font-semibold">{movie.name}</div>
-            <button 
-            onClick={()=>handleRemoveMovie(movie.id)}
-            className="px-4 py-2 text-sm font-semibold text-white/70  bg-red-800 hover:bg-red-900 rounded-3xl">
-                Delete
+            <button
+              onClick={() => handleRemoveMovie(movie.id)}
+              className="px-4 py-2 text-sm font-semibold text-white/70  bg-red-800 hover:bg-red-900 rounded-3xl"
+            >
+              Delete
             </button>
-            </div>
+          </div>
         ))}
       </div>
-      
     </div>
   );
 };
